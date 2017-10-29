2b348f051d21b14869cb311949278cf1cc4a1a6b
alexfigliolia <alexfigliolia@gmail.com>
2017-10-28 22:00:07 -0400

    update

--- server/main.js
+++ server/main.js
@@ -2,7 +2,16 @@ import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { Accounts } from 'meteor/accounts-base';
 import { check } from 'meteor/check';
+import { ReactiveVar } from 'meteor/reactive-var'
 import { BuddyLists } from '../api/buddyList.js';
+import { Conversations } from '../api/conversations.js';
+import { Messages } from '../api/messages.js';
+
+const convos = new ReactiveVar([]);
+
+function updateRV(val) {
+  convos.set(val);
+}
 
 Accounts.onCreateUser((options, user) => {
   user.name = options.name;
@@ -42,6 +51,49 @@ Meteor.publish('buddyLists', function(){
 	} else {
 		return this.ready();
 	}
+});
+
+Meteor.publish('conversations', function() {
+  let currentUser;
+  currentUser = this.userId;
+  if(currentUser) {
+    const convos = Conversations.find({owners: currentUser}, {
+      fields: {
+        label: 1,
+        owners: 1
+      }
+    });
+    return convos;
+  } else {
+    return this.ready();
+  }
+});
+
+Meteor.publish('messages', function() {
+  this.autorun(function(computation) {
+    let currentUser;
+    currentUser = this.userId;
+    if(currentUser) {
+      const cs = Conversations.find({owners: currentUser}).fetch();
+      let chatIds = [];
+      for(let i = 0; i<cs.length; i++) {
+        chatIds.push(cs[i]._id);
+      }
+      updateRV(chatIds);
+      const messages = Messages.find({chatId: {$in: convos.curValue}},{
+        fields: {
+          by: 1,
+          to: 1,
+          text: 1,
+          date: 1,
+          chatId: 1
+        }
+      });
+      return messages;
+    } else {
+      return this.ready();
+    }
+  });
 });
 
 Meteor.methods({
